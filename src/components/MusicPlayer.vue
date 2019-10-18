b75987280d28007e5b6f85b1a51a267385905ebf
Clément Eraud <clement.eraud@gmail.com>
2019-10-18 18:56:14 +0200

    finish music player

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,21 +1,25 @@
 <template>
   <v-card>
-    <v-card-title>{{ playlistName }}</v-card-title>
-    {{currentSong.artist}} - {{currentSong.name}}
+    <v-card-title>
+      <v-col cols="4">{{ playlistName }} : {{currentSongIndex+1}} / {{songs.length}}</v-col>
+      <v-col>
+        <v-progress-linear :value="progressValue"></v-progress-linear>
+      </v-col>
+    </v-card-title>
     <v-card-text>
-      <v-progress-linear :value="progressValue"></v-progress-linear>
+      {{songs ? songs[currentSongIndex].artist : ''}} - {{songs ? songs[currentSongIndex].name : ''}}
     </v-card-text>
     <v-card-actions>
-      <v-btn text icon>
+      <v-btn text icon v-on:click="previous()">
         <v-icon>mdi-skip-previous</v-icon>
       </v-btn>
-      <v-btn text icon>
+      <v-btn text icon v-on:click="pause()">
         <v-icon>mdi-pause</v-icon>
       </v-btn>
       <v-btn text icon v-on:click="play()">
         <v-icon>mdi-play</v-icon>
       </v-btn>
-      <v-btn text icon>
+      <v-btn text icon v-on:click="next()">
         <v-icon>mdi-skip-next</v-icon>
       </v-btn>
     </v-card-actions>
@@ -29,23 +33,50 @@ export default {
   props: ['songs', 'playlistName'],
   data() {
     return {
-      currentSong: {},
+      currentSongIndex: undefined,
+      audio: undefined,
+      progressValue: 0,
+      intervalID: undefined,
     }
   },
-  computed: {
-    progressValue() {
-      return 50;
+  watch: {
+    songs() {
+      this.currentSongIndex = 0;
+    },
+    currentSongIndex(newVal) {
+      clearInterval(this.intervalID);
+      this.progressValue = 0;
+      if (this.audio) {
+        this.audio.pause();
+      }
+      this.audio = new Howl({
+        src: [this.songs[newVal].path],
+      });
+    },
+    progressValue(newVal) {
+      if (newVal >= 100) {
+        this.audio.pause();
+      }
     }
   },
   methods: {
     play() {
-      this.currentSong = this.songs[0];
-      console.log(JSON.parse(JSON.stringify(this.currentSong)));
-      const music = new Howl({
-        src: [this.currentSong.path],
-      });
-      music.play();
+      this.audio.play();
+      // progress bar, +7 every seconds so it will take about 15sec too go to 100.
+      this.intervalID = setInterval(() => {
+        this.progressValue = this.progressValue + 7;
+      }, 1000);
     },
+    pause() {
+      this.audio.pause();
+      clearInterval(this.intervalID);
+    },
+    next() {
+      this.currentSongIndex = this.currentSongIndex === this.songs.length ? this.songs.length : this.currentSongIndex + 1;
+    },
+    previous() {
+      this.currentSongIndex = this.currentSongIndex === 0 ? 0 : this.currentSongIndex - 1;
+    }
   },
 }
 </script>
